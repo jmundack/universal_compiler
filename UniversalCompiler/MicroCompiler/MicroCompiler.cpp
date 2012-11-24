54bd4e6703ccedfc07b6f06570b8290ad2a705e7
Joseph Mundackal <joseph.j.mundackal@gmail.com>
2012-11-24 01:41:38 -0700

    minor clean up

--- UniversalCompiler/MicroCompiler/MicroCompiler.cpp
+++ UniversalCompiler/MicroCompiler/MicroCompiler.cpp
@@ -35,25 +35,21 @@ namespace
       }
       return s;
    }
+}
 
-   void _PrintBlock(const string &action, const string &remainingInput, const deque<string> &ps, const deque<string>& ss,
-         const size_t leftIndex,
-         const size_t rightIndex,
-         const size_t currentIndex,
-         const size_t topIndex)
-   {
-      cout << "******************************************" << endl;
-      cout << action << endl;
-      cout << "Input : " << remainingInput << endl;
-      cout << "PS : ";
-      std::copy(ps.begin(), ps.end(), std::ostream_iterator<string>(std::cout, ""));
-      cout << endl;
-      cout << "SS : ";
-      std::copy(ss.begin(), ss.end(), std::ostream_iterator<string>(std::cout, ""));
-      cout << endl;
-      cout << "Indices : Left(" << leftIndex << ") right (" << rightIndex << ") current(" << currentIndex << ") top(" << topIndex << ")" << endl; 
-      cout << "******************************************" << endl;
-   }
+void MicroCompiler::_PrintBlock(const string &action, const string &remainingInput)
+{
+   cout << "******************************************" << endl;
+   cout << action << endl;
+   cout << "Input : " << remainingInput << endl;
+   cout << "PS : ";
+   std::copy(_ParseStack.begin(), _ParseStack.end(), std::ostream_iterator<string>(std::cout, ""));
+   cout << endl;
+   cout << "SS : ";
+   std::copy(_SemanticStack.begin(), _SemanticStack.end(), std::ostream_iterator<string>(std::cout, ""));
+   cout << endl;
+   cout << "Indices : Left(" << _LeftIndex << ") right (" << _RightIndex << ") current(" << _CurrentIndex << ") top(" << _TopIndex << ")" << endl; 
+   cout << "******************************************" << endl;
 }
 
 MicroCompiler::MicroCompiler(const string &grammer, const string &program):_GrammerFile(grammer),_ProgramFile(program)
@@ -69,19 +65,21 @@ void MicroCompiler::Parse()
    PredictGenerator p(_GrammerFile);
    p.Print();
 
-   deque<string> parseStack, semanticStack;
-   parseStack.push_front("<system_goal>");
-   semanticStack.push_front("<system_goal>");
-   size_t leftIndex(0), rightIndex(0), currentIndex(1), topIndex(2);
+   _ParseStack.push_front("<system_goal>");
+   _SemanticStack.push_front("<system_goal>");
+   _LeftIndex = 0;
+   _RightIndex = 0;
+   _CurrentIndex = 1;
+   _TopIndex = 2;
 
    stack<string> s;
    s.push("<system_goal>");
    cout << "***********************************************" << endl;
-   _PrintBlock("Enrty", remainingInput, parseStack, semanticStack, leftIndex, rightIndex, currentIndex, topIndex);
-   while(!parseStack.empty())
+   _PrintBlock("Enrty", remainingInput);
+   while(!_ParseStack.empty())
    {
       string action("INVALID");
-      string topSymbol = parseStack.at(0);
+      string topSymbol = _ParseStack.at(0);
       if (topSymbol.at(0) == '<')
       {
          const pair<string,string> key(make_pair(topSymbol, _GetTokenStr(scanner.GetNextToken())));
@@ -95,29 +93,29 @@ void MicroCompiler::Parse()
          else
          {
             action = "NonTerm : T(" + topSymbol + "," + key.second + ") = " + boost::lexical_cast<string>(productionNum);
-            parseStack.pop_front();
-            parseStack.push_front(boost::lexical_cast<string>(topIndex));
-            parseStack.push_front(boost::lexical_cast<string>(currentIndex));
-            parseStack.push_front(boost::lexical_cast<string>(rightIndex));
-            parseStack.push_front(boost::lexical_cast<string>(leftIndex));
-            parseStack.push_front("EOP");
+            _ParseStack.pop_front();
+            _ParseStack.push_front(boost::lexical_cast<string>(_TopIndex));
+            _ParseStack.push_front(boost::lexical_cast<string>(_CurrentIndex));
+            _ParseStack.push_front(boost::lexical_cast<string>(_RightIndex));
+            _ParseStack.push_front(boost::lexical_cast<string>(_LeftIndex));
+            _ParseStack.push_front("EOP");
             if(Debug) cout << "************** Getting production : " << productionNum << " of " << (productions.size() + 1) << endl;
             const pair<string, vector<string> > &production(productions.at(productionNum -1));
             if(Debug) cout << "************* Getting rhs of production" << endl;
             const vector<string> &rhs(production.second);
             for(vector<string>::const_reverse_iterator itr = rhs.rbegin(); itr != rhs.rend(); ++itr)
-               parseStack.push_front(*itr);
+               _ParseStack.push_front(*itr);
             size_t numNonActionSymbols(0);
             for(vector<string>::const_iterator itr = rhs.begin(); itr != rhs.end(); ++itr)
                if (itr->at(0) != '#') // skip action symbols
                {
                   numNonActionSymbols++;
-                  semanticStack.push_front(*itr);
+                  _SemanticStack.push_front(*itr);
                }
-            leftIndex = currentIndex;
-            rightIndex = topIndex;
-            currentIndex = rightIndex;
-            topIndex = topIndex + numNonActionSymbols;
+            _LeftIndex = _CurrentIndex;
+            _RightIndex = _TopIndex;
+            _CurrentIndex = _RightIndex;
+            _TopIndex += numNonActionSymbols;
          }
       }
       else
@@ -125,46 +123,46 @@ void MicroCompiler::Parse()
          const Symbols &terminalSymbols(p.GetTerminalSymbols());
          if (terminalSymbols.find(topSymbol) != terminalSymbols.end() || topSymbol == "lamda")
          {
-            action = "Term : " + parseStack.front();
+            action = "Term : " + _ParseStack.front();
             if (topSymbol != "lamda")
             {
-               deque<string>::iterator itr = semanticStack.end() - currentIndex;
+               deque<string>::iterator itr = _SemanticStack.end() - _CurrentIndex;
                *itr = "[" + scanner.GetBuffer() + "]";
                remainingInput = scanner.GetRemainingInput();
                scanner.ReadNextToken();
             }
             if(Debug) cout << "Poping for match" << endl;
-            currentIndex ++;
-            parseStack.pop_front();
+            _CurrentIndex++;
+            _ParseStack.pop_front();
          }
          else if (topSymbol == "EOP")
          {
-            action = "EOP : (" + parseStack.at(1) + ',' + parseStack.at(2) +',' + parseStack.at(3) +','+ parseStack.at(4) + ")";
-            parseStack.pop_front(); // pop EOP
+            action = "EOP : (" + _ParseStack.at(1) + ',' + _ParseStack.at(2) +',' + _ParseStack.at(3) +','+ _ParseStack.at(4) + ")";
+            _ParseStack.pop_front(); // pop EOP
             // restore all indices
-            leftIndex = boost::lexical_cast<size_t>(parseStack.front());
-            parseStack.pop_front();
-            rightIndex = boost::lexical_cast<size_t>(parseStack.front());
-            parseStack.pop_front();
-            currentIndex = boost::lexical_cast<size_t>(parseStack.front());
-            parseStack.pop_front();
-            topIndex = boost::lexical_cast<size_t>(parseStack.front());
-            parseStack.pop_front();
+            _LeftIndex = boost::lexical_cast<size_t>(_ParseStack.front());
+            _ParseStack.pop_front();
+            _RightIndex = boost::lexical_cast<size_t>(_ParseStack.front());
+            _ParseStack.pop_front();
+            _CurrentIndex = boost::lexical_cast<size_t>(_ParseStack.front());
+            _ParseStack.pop_front();
+            _TopIndex = boost::lexical_cast<size_t>(_ParseStack.front());
+            _ParseStack.pop_front();
             // Done restoring indices
-            currentIndex ++;
-            while(semanticStack.size() != topIndex -1)
+            _CurrentIndex ++;
+            while(_SemanticStack.size() != _TopIndex -1)
             {
-               cout << "Popping : " << semanticStack.front()  << " from semantic stack " << endl;
-               semanticStack.pop_front();
+               cout << "Popping : " << _SemanticStack.front()  << " from _Semantic stack " << endl;
+               _SemanticStack.pop_front();
             }
          }
          else
          {
-            action = "Action : " + parseStack.front();
-            cout << "Calling : " << parseStack.front() << endl;
-            parseStack.pop_front();
+            action = "Action : " + _ParseStack.front();
+            cout << "Calling : " << _ParseStack.front() << endl;
+            _ParseStack.pop_front();
          }
       }
-      _PrintBlock(action, remainingInput, parseStack, semanticStack, leftIndex, rightIndex, currentIndex, topIndex);
+      _PrintBlock(action, remainingInput);
    }
 }
--- UniversalCompiler/MicroCompiler/MicroCompiler.h
+++ UniversalCompiler/MicroCompiler/MicroCompiler.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <deque>
 
 class MicroCompiler
 {
@@ -7,6 +8,10 @@ public:
    MicroCompiler(const std::string &grammer, const std::string &program);
    void Parse();
 private:
+   void _PrintBlock(const std::string &action, const std::string &remainingInput);
    std::string _GrammerFile;
    std::string _ProgramFile;
+   std::deque<std::string> _ParseStack;
+   std::deque<std::string> _SemanticStack;
+   size_t _LeftIndex, _RightIndex, _CurrentIndex, _TopIndex;
 };
